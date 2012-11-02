5f67e09df6cd1fd55a23cdda723b9104e1ead016
Vadim Godunko <vgodunko@gmail.com>
2012-11-02 10:55:58 +0000

    Add To_Binary_Message_Digest subprogram to workaround missing GNAT.SHA1.Digest subprogram.

--- design/soap/ws-security/web_services-soap-security-modules.adb
+++ design/soap/ws-security/web_services-soap-security-modules.adb
@@ -72,6 +72,12 @@ package body Web_Services.SOAP.Security.Modules is
     (Item : Ada.Streams.Stream_Element_Array)
        return Ada.Streams.Stream_Element_Array;
 
+   function To_Binary_Message_Digest
+    (Item : String) return Ada.Streams.Stream_Element_Array;
+   --  Converts string into array of stream elements. Note: this subprogram is
+   --  needed for GNAT GPL 2012 only, because it doesn't provide
+   --  GNAT.SHA1.Digest subprogram which returns Binary_Message_Digest.
+
    ----------------------
    -- Default_Provider --
    ----------------------
@@ -284,7 +290,12 @@ package body Web_Services.SOAP.Security.Modules is
                 & UTF8_Codec.Encode
                    (Token.Created & Password).To_Stream_Element_Array;
          Digest    : constant League.Strings.Universal_String
-           := UTF8_Codec.Decode (To_Base_64 (GNAT.SHA1.Digest (Full_Data)));
+           := UTF8_Codec.Decode
+               (To_Base_64
+                 (To_Binary_Message_Digest (GNAT.SHA1.Digest (Full_Data))));
+         --  GNAT GPL 2012: Use of To_Binary_Message_Digest is not needed here,
+         --  but GNAT GPL 2012 doesn't provide corresponding GNAT.SHA1.Digest
+         --  subprogram.
 
       begin
          if Digest /= Token.Password then
@@ -437,5 +448,52 @@ package body Web_Services.SOAP.Security.Modules is
 
       return Result (Result'First .. Unused - 1);
    end To_Base_64;
+
+   ------------------------------
+   -- To_Binary_Message_Digest --
+   ------------------------------
+
+   function To_Binary_Message_Digest
+    (Item : String) return Ada.Streams.Stream_Element_Array
+   is
+      use type Ada.Streams.Stream_Element;
+      use type Ada.Streams.Stream_Element_Offset;
+
+      function Decode (Item : Character) return Ada.Streams.Stream_Element;
+
+      ------------
+      -- Decode --
+      ------------
+
+      function Decode (Item : Character) return Ada.Streams.Stream_Element is
+      begin
+         case Item is
+            when '0' .. '9' =>
+               return Character'Pos (Item) - Character'Pos ('0');
+
+            when 'a' .. 'f' =>
+               return Character'Pos (Item) - Character'Pos ('a') + 10;
+
+            when 'A' .. 'F' =>
+               return Character'Pos (Item) - Character'Pos ('A') + 10;
+
+            when others =>
+               raise Constraint_Error;
+         end case;
+      end Decode;
+
+      Result : Ada.Streams.Stream_Element_Array (1 .. Item'Length / 2);
+      First  : Natural := Item'First;
+      Unused : Ada.Streams.Stream_Element_Offset := Result'First;
+
+   begin
+      while Item'Last - First > 0 loop
+         Result (Unused) := (Decode (Item (First)) * 16) or Decode (Item (First + 1));
+         Unused := Unused + 1;
+         First := First + 2;
+      end loop;
+
+      return Result;
+   end To_Binary_Message_Digest;
 
 end Web_Services.SOAP.Security.Modules;
