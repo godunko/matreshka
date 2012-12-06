c00602a77a96f1a11bd7ba77fdccc071b158c497
Vadim Godunko <vgodunko@gmail.com>
2012-12-06 12:00:22 +0000

    Output namespaces of loaded model.

--- design/schema/driver/driver.adb
+++ design/schema/driver/driver.adb
@@ -1,4 +1,7 @@
+with Ada.Wide_Wide_Text_IO;
+
 with League.Application;
+with League.String_Vectors;
 
 with XML.Schema.Models;
 with XML.Schema.Utilities;
@@ -6,11 +9,26 @@ with XML.Schema.Utilities;
 with Matreshka.XML_Schema.URI_Rewriter;
 
 procedure Driver is
-   Model : XML.Schema.Models.XS_Model;
+   Model      : XML.Schema.Models.XS_Model;
+   Namespaces : League.String_Vectors.Universal_String_Vector;
 
 begin
    Matreshka.XML_Schema.URI_Rewriter.Initialize;
 
    Model :=
      XML.Schema.Utilities.Load (League.Application.Arguments.Element (1));
+
+   if Model.Is_Null then
+      raise Program_Error;
+   end if;
+
+   Ada.Wide_Wide_Text_IO.Put_Line ("Namespaces:");
+   Namespaces := Model.Get_Namespaces;
+
+   for J in 1 .. Namespaces.Length loop
+      Ada.Wide_Wide_Text_IO.Put_Line
+       ("   '"
+          & Namespaces (J).To_Wide_Wide_String
+          & ''');
+   end loop;
 end Driver;
