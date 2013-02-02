5ed1b2106273469a19f2e4a22565d7a97511d6a1
Vadim Godunko <vgodunko@gmail.com>
2013-02-02 22:56:14 +0000

    New implementation of ODF DOM iterator.

--- design/odf/odf/odf-dom-iterators.ads
+++ design/odf/odf/odf-dom-iterators.ads
@@ -95,247 +95,247 @@ package ODF.DOM.Iterators is
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Automatic_Styles.ODF_Office_Automatic_Styles_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Body
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Bodies.ODF_Office_Body_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Document_Content
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Document_Content.ODF_Office_Document_Content_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Document_Styles
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Document_Styles.ODF_Office_Document_Styles_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Font_Face_Decls
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Font_Face_Decls.ODF_Office_Font_Face_Decls_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Master_Styles
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Master_Styles.ODF_Office_Master_Styles_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Scripts
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Scripts.ODF_Office_Scripts_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Styles
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Styles.ODF_Office_Styles_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Office_Text
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Office.Text.ODF_Office_Text_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Background_Image
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Background_Image.ODF_Style_Background_Image_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Default_Style
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Default_Style.ODF_Style_Default_Style_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Font_Face
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Font_Face.ODF_Style_Font_Face_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Footer_Style
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Footer_Style.ODF_Style_Footer_Style_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Footnote_Sep
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Footnote_Sep.ODF_Style_Footnote_Sep_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Graphic_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Graphic_Properties.ODF_Style_Graphic_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Header_Style
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Header_Style.ODF_Style_Header_Style_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_List_Level_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.List_Level_Properties.ODF_Style_List_Level_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Master_Page
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Master_Page.ODF_Style_Master_Page_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Page_Layout
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Page_Layout.ODF_Style_Page_Layout_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Page_Layout_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Page_Layout_Properties.ODF_Style_Page_Layout_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Paragraph_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Paragraph_Properties.ODF_Style_Paragraph_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Style
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Style.ODF_Style_Style_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Tab_Stops
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Tab_Stops.ODF_Style_Tab_Stops_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Table_Cell_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Table_Cell_Properties.ODF_Style_Table_Cell_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Table_Column_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Table_Column_Properties.ODF_Style_Table_Column_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Table_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Table_Properties.ODF_Style_Table_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Table_Row_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Table_Row_Properties.ODF_Style_Table_Row_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Style_Text_Properties
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Style.Text_Properties.ODF_Style_Text_Properties_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Table_Covered_Table_Cell
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Table.Covered_Table_Cell.ODF_Table_Covered_Table_Cell_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Table_Table
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Table.Table.ODF_Table_Table_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Table_Table_Cell
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Table.Table_Cell.ODF_Table_Table_Cell_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Table_Table_Column
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Table.Table_Column.ODF_Table_Table_Column_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Table_Table_Row
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Table.Table_Row.ODF_Table_Table_Row_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_Linenumbering_Configuration
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.Linenumbering_Configuration.ODF_Text_Linenumbering_Configuration_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_Notes_Configuration
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.Notes_Configuration.ODF_Text_Notes_Configuration_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_Outline_Level_Style
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.Outline_Level_Style.ODF_Text_Outline_Level_Style_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_Outline_Style
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.Outline_Style.ODF_Text_Outline_Style_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_P
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.P.ODF_Text_P_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_Sequence_Decl
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.Sequence_Decl.ODF_Text_Sequence_Decl_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_Sequence_Decls
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.Sequence_Decls.ODF_Text_Sequence_Decls_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
    not overriding procedure Visit_Text_Span
     (Self    : in out ODF_Iterator;
      Visitor : in out XML.DOM.Visitors.Abstract_Visitor'Class;
      Element : not null ODF.DOM.Elements.Text.Span.ODF_Text_Span_Access;
-     Control : in out XML.DOM.Visitors.Traverse_Control) is null;
+     Control : in out XML.DOM.Visitors.Traverse_Control);
 
 private
 
