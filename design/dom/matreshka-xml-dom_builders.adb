22b68a3210a8b79e91da611e78d2b7adf0851f94
Vadim Godunko <vgodunko@gmail.com>
2013-04-23 14:27:56 +0000

    Update copyright headers.

--- design/dom/matreshka-xml-dom_builders.adb
+++ design/dom/matreshka-xml-dom_builders.adb
@@ -8,7 +8,7 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --                                                                          --
--- Copyright © 2011, Vadim Godunko <vgodunko@gmail.com>                     --
+-- Copyright © 2011-2013, Vadim Godunko <vgodunko@gmail.com>                --
 -- All rights reserved.                                                     --
 --                                                                          --
 -- Redistribution and use in source and binary forms, with or without       --
@@ -40,9 +40,6 @@
 --                                                                          --
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
-------------------------------------------------------------------------------
---  This package provides DOM tree builder. It should be connected to SAX
---  reader to construct DOM tree.
 ------------------------------------------------------------------------------
 
 package body Matreshka.XML.DOM_Builders is
