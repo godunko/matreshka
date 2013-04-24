27537b8e695f54396b6229955df224f58fd00aa9
Vadim Godunko <vgodunko@gmail.com>
2013-04-24 16:14:41 +0000

    Add Internals packages for attributes.

---
+++ design/odf/odf/odf-dom-attributes-style-vertical_align-internals.adb
@@ -0,0 +1,75 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                          Open Document Toolkit                           --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2013, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with Matreshka.DOM_Nodes;
+with XML.DOM.Attributes.Internals;
+
+package body ODF.DOM.Attributes.Style.Vertical_Align.Internals is
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create
+    (Node : Matreshka.ODF_Attributes.Style.Vertical_Align.Style_Vertical_Align_Access)
+       return ODF.DOM.Attributes.Style.Vertical_Align.ODF_Style_Vertical_Align is
+   begin
+      return
+       (XML.DOM.Attributes.Internals.Create
+         (Matreshka.DOM_Nodes.Attribute_Access (Node)) with null record);
+   end Create;
+
+   ----------
+   -- Wrap --
+   ----------
+
+   function Wrap
+    (Node : Matreshka.ODF_Attributes.Style.Vertical_Align.Style_Vertical_Align_Access)
+       return ODF.DOM.Attributes.Style.Vertical_Align.ODF_Style_Vertical_Align is
+   begin
+      return
+       (XML.DOM.Attributes.Internals.Wrap
+         (Matreshka.DOM_Nodes.Attribute_Access (Node)) with null record);
+   end Wrap;
+
+end ODF.DOM.Attributes.Style.Vertical_Align.Internals;
