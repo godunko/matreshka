0fc557c4b3315ec815bb4e47f44994cc868f4108
Artem Kuraleh <artem.kuraleh>
2013-11-11 14:25:59 +0000

    xsd_to_ada_generator/xsd_to_ada-encoder.adb Удалил повторный вывод текста.

--- design/xsd_to_ada_generator/xsd_to_ada-encoder.adb
+++ design/xsd_to_ada_generator/xsd_to_ada-encoder.adb
@@ -604,11 +604,6 @@ package body XSD_To_Ada.Encoder is
 
       Create_Element_Type (Model, Payload_Writer);
 
-      Ada.Wide_Wide_Text_IO.Put (Element_Name.Text.To_Wide_Wide_String);
-      Ada.Wide_Wide_Text_IO.Put_Line
-       (Encoder_Top_Writer.Text.To_Wide_Wide_String);
-      Ada.Wide_Wide_Text_IO.Put (Payload_Writer.Text.To_Wide_Wide_String);
-
       Writers.P (Payload_Writer, "begin");
 
 --      Ada.Wide_Wide_Text_IO.Put_Line ("begin");
