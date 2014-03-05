5b6cc07dbc3ccc36f9716d241b99a6621472c302
Vadim Godunko <vgodunko@gmail.com>
2014-03-05 18:26:51 +0000

    Generate implmentation of attributes of 'draw' module.

---
+++ design/odf2/odf/draw/matreshka-odf_draw-caption_gap_attributes.ads
@@ -0,0 +1,61 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                          Open Document Toolkit                           --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2014, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with ODF.DOM.Draw_Caption_Gap_Attributes;
+
+package Matreshka.ODF_Draw.Caption_Gap_Attributes is
+
+   type Draw_Caption_Gap_Attribute_Node is
+     new Matreshka.ODF_Draw.Abstract_Draw_Attribute_Node
+       and ODF.DOM.Draw_Caption_Gap_Attributes.ODF_Draw_Caption_Gap_Attribute
+         with null record;
+
+   overriding function Create
+    (Parameters : not null access Matreshka.DOM_Attributes.Attribute_L2_Parameters)
+       return Draw_Caption_Gap_Attribute_Node;
+
+   overriding function Get_Local_Name
+    (Self : not null access constant Draw_Caption_Gap_Attribute_Node)
+       return League.Strings.Universal_String;
+
+end Matreshka.ODF_Draw.Caption_Gap_Attributes;
