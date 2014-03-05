cb18b012c2da1e8ddb4b8f602683959447f0b448
Vadim Godunko <vgodunko@gmail.com>
2014-03-05 20:03:44 +0000

    Regenerate code.

--- design/odf2/odf/script/odf-dom-script_event_listener_elements.ads
+++ design/odf2/odf/script/odf-dom-script_event_listener_elements.ads
@@ -51,6 +51,7 @@ package ODF.DOM.Script_Event_Listener_Elements is
      and XML.DOM.Elements.DOM_Element;
 
    type ODF_Script_Event_Listener_Element_Access is
-     access all ODF_Script_Event_Listener_Element'Class;
+     access all ODF_Script_Event_Listener_Element'Class
+       with Storage_Size => 0;
 
 end ODF.DOM.Script_Event_Listener_Elements;
