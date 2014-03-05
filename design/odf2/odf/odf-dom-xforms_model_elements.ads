6c8cf02545a5d343f051387c709ffddffd813a77
Vadim Godunko <vgodunko@gmail.com>
2014-03-05 20:04:55 +0000

    Regenerate code.

--- design/odf2/odf/odf-dom-xforms_model_elements.ads
+++ design/odf2/odf/odf-dom-xforms_model_elements.ads
@@ -51,6 +51,7 @@ package ODF.DOM.Xforms_Model_Elements is
      and XML.DOM.Elements.DOM_Element;
 
    type ODF_Xforms_Model_Element_Access is
-     access all ODF_Xforms_Model_Element'Class;
+     access all ODF_Xforms_Model_Element'Class
+       with Storage_Size => 0;
 
 end ODF.DOM.Xforms_Model_Elements;
