1d900c3b12e7586ae1800b6cc1f2434c4c46e7ad
Vadim Godunko <vgodunko@gmail.com>
2014-03-05 20:03:32 +0000

    Regenerate code.

--- design/odf2/odf/math/odf-dom-math_math_elements.ads
+++ design/odf2/odf/math/odf-dom-math_math_elements.ads
@@ -51,6 +51,7 @@ package ODF.DOM.Math_Math_Elements is
      and XML.DOM.Elements.DOM_Element;
 
    type ODF_Math_Math_Element_Access is
-     access all ODF_Math_Math_Element'Class;
+     access all ODF_Math_Math_Element'Class
+       with Storage_Size => 0;
 
 end ODF.DOM.Math_Math_Elements;
