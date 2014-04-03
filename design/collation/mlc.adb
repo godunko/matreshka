1b73159c43b7c6b472568a3013916c7611fc0e9e
Vadim Godunko <vgodunko@gmail.com>
2014-04-03 10:06:21 +0000

    Minimal implementation of CLDR collation parser.
    Refs #352.

--- design/collation/mlc.adb
+++ design/collation/mlc.adb
@@ -1,8 +1,24 @@
+with League.Strings;
+with XML.SAX.File_Input_Sources;
+with XML.SAX.Simple_Readers;
+
 with AllKeys_Reader;
+with CLDR_Parsers;
 
 procedure MLC is
    Collations : AllKeys_Reader.Collation_Information_Access;
 
+   Input      : aliased XML.SAX.File_Input_Sources.File_Input_Source;
+   Parser     : aliased CLDR_Parsers.CLDR_Parser;
+   Reader     : XML.SAX.Simple_Readers.Simple_Reader;
+
 begin
    Collations := AllKeys_Reader.Load_AllKeys_File;
+
+   Input.Open_By_File_Name
+    (League.Strings.To_Universal_String
+      ("../../data/cldr/25/common/collation/uk.xml"));
+   Reader.Set_Content_Handler (Parser'Unchecked_Access);
+   Reader.Parse (Input'Unchecked_Access);
+   Input.Close;
 end MLC;
