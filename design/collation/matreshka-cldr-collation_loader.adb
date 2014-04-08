471aa107512988844b9496c08c21016912cebda1
Vadim Godunko <vgodunko@gmail.com>
2014-04-08 16:29:53 +0000

    Extend collation data loader to use environment variable and precompiled prefix dir to lookup for CLDR data files.
    Refs #352.

--- design/collation/matreshka-cldr-collation_loader.adb
+++ design/collation/matreshka-cldr-collation_loader.adb
@@ -41,6 +41,10 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
+with Ada.Directories;
+
+with League.Application;
+with Matreshka.Config;
 with Matreshka.CLDR.AllKeys_Reader;
 with Matreshka.CLDR.Collation_Compiler;
 with Matreshka.CLDR.Collation_Data;
@@ -50,16 +54,81 @@ with XML.SAX.Simple_Readers;
 
 package body Matreshka.CLDR.Collation_Loader is
 
-   CLDR_Root      : constant League.Strings.Universal_String
-     := League.Strings.To_Universal_String
-          ("/home/godunko/Matreshka/matreshka/data/cldr/25");
-   AllKeys_Path   : constant League.Strings.Universal_String
+   use type League.Strings.Universal_String;
+
+   CDLR_Root_Name        : constant League.Strings.Universal_String
+     := League.Strings.To_Universal_String ("MATRESHKA_CLDR_DIR");
+   CLDR_Root             : constant League.Strings.Universal_String
+     := Matreshka.Config.Prefix
+          & League.Strings.To_Universal_String ("/share/matreshka/cldr/");
+   AllKeys_Relative_Path : constant League.Strings.Universal_String
      := League.Strings.To_Universal_String ("/common/uca/allkeys_CLDR.txt");
-   --  Path to allkeys.txt file relative to root CLDR directory.
+   --  Path to allkeys.txt file relative to root of CLDR directory.
    Collation_Root : constant League.Strings.Universal_String
      := League.Strings.To_Universal_String ("/common/collation/");
    XML_Suffix     : constant League.Strings.Universal_String
      := League.Strings.To_Universal_String (".xml");
+
+   function AllKeys_Path
+    (Root : League.Strings.Universal_String)
+       return League.Strings.Universal_String;
+
+   function Collation_Path
+    (Root     : League.Strings.Universal_String;
+     Language : League.Strings.Universal_String)
+       return League.Strings.Universal_String;
+
+   ------------------
+   -- AllKeys_Path --
+   ------------------
+
+   function AllKeys_Path
+    (Root : League.Strings.Universal_String)
+       return League.Strings.Universal_String is
+   begin
+      return Root & AllKeys_Relative_Path;
+   end AllKeys_Path;
+
+   --------------------
+   -- Collation_Path --
+   --------------------
+
+   function Collation_Path
+    (Root     : League.Strings.Universal_String;
+     Language : League.Strings.Universal_String)
+       return League.Strings.Universal_String is
+   begin
+      return Root & Collation_Root & Language & XML_Suffix;
+   end Collation_Path;
+
+   -------------------------
+   -- Load_Collation_Data --
+   -------------------------
+
+   procedure Load_Collation_Data
+    (Language : League.Strings.Universal_String;
+     Locale   : not null access Matreshka.Internals.Locales.Locale_Data;
+     Location : League.Strings.Universal_String)
+   is
+      Data   : Matreshka.CLDR.Collation_Data.Collation_Information_Access;
+      Input  : aliased XML.SAX.File_Input_Sources.File_Input_Source;
+      Parser : aliased Matreshka.CLDR.LDML_Parsers.LDML_Parser;
+      Reader : XML.SAX.Simple_Readers.Simple_Reader;
+
+   begin
+      Data :=
+        Matreshka.CLDR.AllKeys_Reader.Load_AllKeys_File
+         (AllKeys_Path (Location));
+
+      Input.Open_By_File_Name (Collation_Path (Location, Language));
+      Parser.Collations := Data;
+      Reader.Set_Content_Handler (Parser'Unchecked_Access);
+      Reader.Parse (Input'Unchecked_Access);
+      Input.Close;
+
+      Matreshka.CLDR.Collation_Compiler.Construct_Collation_Information
+       (Data.all, Locale);
+   end Load_Collation_Data;
 
    -------------------------
    -- Load_Collation_Data --
@@ -69,27 +138,16 @@ package body Matreshka.CLDR.Collation_Loader is
     (Language : League.Strings.Universal_String;
      Locale   : not null access Matreshka.Internals.Locales.Locale_Data)
    is
-      use type League.Strings.Universal_String;
-
-      Data   : Matreshka.CLDR.Collation_Data.Collation_Information_Access;
-      Input  : aliased XML.SAX.File_Input_Sources.File_Input_Source;
-      Parser : aliased Matreshka.CLDR.LDML_Parsers.LDML_Parser;
-      Reader : XML.SAX.Simple_Readers.Simple_Reader;
+      Env : constant League.Strings.Universal_String
+        := League.Application.Environment.Value (CDLR_Root_Name);
 
    begin
-      Data :=
-        Matreshka.CLDR.AllKeys_Reader.Load_AllKeys_File
-         (CLDR_Root & AllKeys_Path);
+      if not Env.Is_Empty then
+         Load_Collation_Data (Language, Locale, Env);
 
-      Input.Open_By_File_Name
-       (CLDR_Root & Collation_Root & Language & XML_Suffix);
-      Parser.Collations := Data;
-      Reader.Set_Content_Handler (Parser'Unchecked_Access);
-      Reader.Parse (Input'Unchecked_Access);
-      Input.Close;
-
-      Matreshka.CLDR.Collation_Compiler.Construct_Collation_Information
-       (Data.all, Locale);
+      else
+         Load_Collation_Data (Language, Locale, CLDR_Root);
+      end if;
    end Load_Collation_Data;
 
 end Matreshka.CLDR.Collation_Loader;
