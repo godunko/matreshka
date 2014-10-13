0563497aec69b6eba20e41e16f79cd93f050f0d8
Vadim Godunko <vgodunko@gmail.com>
2014-10-13 19:56:42 +0000

    Extent example application to object Document object.

--- design/asis2js/examples/hello_world.adb
+++ design/asis2js/examples/hello_world.adb
@@ -1,4 +1,9 @@
+with WebAPI.HTML.Documents;
+with WebAPI.HTML.Globals; use WebAPI.HTML.Globals;
+
 procedure Hello_World is
+   D : constant WebAPI.HTML.Documents.Document_Access
+     := Window.Get_Document;
 begin
    null;
 end Hello_World;
