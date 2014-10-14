510c7548b7f29e6d913a68a51024985f45a2b4f5
Vadim Godunko <vgodunko@gmail.com>
2014-10-14 22:09:55 +0000

    Remove commented out code.

--- design/asis2js/examples/hello_world.adb
+++ design/asis2js/examples/hello_world.adb
@@ -2,7 +2,6 @@ with League.Strings;
 
 with WebAPI.DOM.Elements;
 with WebAPI.DOM.Texts;
---with WebAPI.HTML.Canvas_Elements;
 with WebAPI.HTML.Documents;
 with WebAPI.HTML.Globals; use WebAPI.HTML.Globals;
 
@@ -14,9 +13,6 @@ procedure Hello_World is
 
    D : constant WebAPI.HTML.Documents.Document_Access
      := Window.Get_Document;
---   C : constant WebAPI.HTML.Canvas_Elements.HTML_Canvas_Element_Access
---     := WebAPI.HTML.Canvas_Elements.HTML_Canvas_Element_Access
---         (D.Get_Element_By_Id (League.Strings.To_Universal_String ("canvas")));
    P : constant WebAPI.DOM.Elements.Element_Access
      := D.Get_Element_By_Id (+"text");
    T : constant WebAPI.DOM.Texts.Text_Access
