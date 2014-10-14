2060b66a2ea2d9ea01ed938170fcb1cccf90bd2e
Vadim Godunko <vgodunko@gmail.com>
2014-10-14 18:15:45 +0000

    Derive DOM::Element and DOM::Text from DOM::Node.

--- design/asis2js/webapi/dom/webapi-dom-elements.ads
+++ design/asis2js/webapi/dom/webapi-dom-elements.ads
@@ -1,7 +1,9 @@
+with WebAPI.DOM.Nodes;
 
 package WebAPI.DOM.Elements is
 
-   type Element is limited interface;
+   type Element is limited interface
+     and WebAPI.DOM.Nodes.Node;
 
    type Element_Access is access all Element'Class
      with Storage_Size => 0;
--- design/asis2js/webapi/dom/webapi-dom-texts.ads
+++ design/asis2js/webapi/dom/webapi-dom-texts.ads
@@ -1,7 +1,9 @@
+with WebAPI.DOM.Nodes;
 
 package WebAPI.DOM.Texts is
 
-   type Text is limited interface;
+   type Text is limited interface
+     and WEbAPI.DOM.Nodes.Node;  -- XXX Must be derived from CharacterData!
 
    type Text_Access is access all Text'Class
      with Storage_Size => 0;
