c8c054a0fbca2ddf40ac0b57cf4efcd3dc63242d
Vadim Godunko <vgodunko@gmail.com>
2014-10-14 22:14:45 +0000

    Add WebGL example.

---
+++ design/asis2js/examples/gldraw.adb
@@ -0,0 +1,29 @@
+with League.Strings;
+
+with WebAPI.HTML.Canvas_Elements;
+with WebAPI.HTML.Documents;
+with WebAPI.HTML.Globals; use WebAPI.HTML.Globals;
+with WebAPI.WebGL.Rendering_Contexts;
+
+procedure GLDraw is
+
+   use type WebAPI.WebGL.Rendering_Contexts.WebGL_Rendering_Context_Access;
+
+   function "+"
+    (Item : Wide_Wide_String) return League.Strings.Universal_String
+       renames League.Strings.To_Universal_String;
+
+   D : constant WebAPI.HTML.Documents.Document_Access
+     := Window.Get_Document;
+   C : constant WebAPI.HTML.Canvas_Elements.HTML_Canvas_Element_Access
+     := WebAPI.HTML.Canvas_Elements.HTML_Canvas_Element_Access
+         (D.Get_Element_By_Id (League.Strings.To_Universal_String ("canvas")));
+   G : constant WebAPI.WebGL.Rendering_Contexts.WebGL_Rendering_Context_Access
+     := WebAPI.WebGL.Rendering_Contexts.WebGL_Rendering_Context_Access
+         (C.Get_Context (+"webgl"));
+
+begin
+   if G = null then
+      return;
+   end if;
+end GLDraw;
