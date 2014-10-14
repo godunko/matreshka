2f2c60964d57d6b97ba65bed8c72c34c86412758
Vadim Godunko <vgodunko@gmail.com>
2014-10-14 19:54:48 +0000

    Use renaming of function to simplify code.

--- design/asis2js/examples/hello_world.adb
+++ design/asis2js/examples/hello_world.adb
@@ -7,15 +7,20 @@ with WebAPI.HTML.Documents;
 with WebAPI.HTML.Globals; use WebAPI.HTML.Globals;
 
 procedure Hello_World is
+
+   function "+"
+    (Item : Wide_Wide_String) return League.Strings.Universal_String
+       renames League.Strings.To_Universal_String;
+
    D : constant WebAPI.HTML.Documents.Document_Access
      := Window.Get_Document;
 --   C : constant WebAPI.HTML.Canvas_Elements.HTML_Canvas_Element_Access
 --     := WebAPI.HTML.Canvas_Elements.HTML_Canvas_Element_Access
 --         (D.Get_Element_By_Id (League.Strings.To_Universal_String ("canvas")));
    P : constant WebAPI.DOM.Elements.Element_Access
-     := D.Get_Element_By_Id (League.Strings.To_Universal_String ("text"));
+     := D.Get_Element_By_Id (+"text");
    T : constant WebAPI.DOM.Texts.Text_Access
-     := D.Create_Text_Node (League.Strings.To_Universal_String ("Hello, world!"));
+     := D.Create_Text_Node (+"Hello, world!");
 
 begin
    P.Append_Child (T);
