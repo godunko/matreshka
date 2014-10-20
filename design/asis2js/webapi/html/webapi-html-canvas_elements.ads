060b33d0d9facb6ba324c65ea8d3fdb75903c031
Vadim Godunko <vgodunko@gmail.com>
2014-10-20 22:16:08 +0000

    More binding of HTMLCanvasElement.

--- design/asis2js/webapi/html/webapi-html-canvas_elements.ads
+++ design/asis2js/webapi/html/webapi-html-canvas_elements.ads
@@ -41,6 +41,8 @@
 ------------------------------------------------------------------------------
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
+--  This package provides binding for interface HTMLCanvasElement.
+------------------------------------------------------------------------------
 with League.Strings;
 
 with WebAPI.HTML.Elements;
@@ -54,13 +56,70 @@ package WebAPI.HTML.Canvas_Elements is
    type HTML_Canvas_Element_Access is access all HTML_Canvas_Element'Class
       with Storage_Size => 0;
 
+   not overriding function Get_Width
+    (Self : not null access constant HTML_Canvas_Element)
+       return Natural is abstract
+         with Import        => True,
+              Convention    => JavaScript_Property_Getter,
+              External_Name => "width";
+
+   not overriding procedure Set_Width
+    (Self : not null access HTML_Canvas_Element;
+     To   : Natural) is abstract
+       with Import        => True,
+            Convention    => JavaScript_Property_Setter,
+            External_Name => "width";
+
+   not overriding function Get_Height
+    (Self : not null access constant HTML_Canvas_Element)
+       return Natural is abstract
+         with Import        => True,
+              Convention    => JavaScript_Property_Getter,
+              External_Name => "height";
+
+   not overriding procedure Set_Height
+    (Self : not null access HTML_Canvas_Element;
+     To   : Natural) is abstract
+       with Import        => True,
+            Convention    => JavaScript_Property_Setter,
+            External_Name => "height";
+
    not overriding function Get_Context
     (Self       : not null access HTML_Canvas_Element;
      Context_Id : League.Strings.Universal_String)
        return WebAPI.HTML.Rendering_Contexts.Rendering_Context_Access
          is abstract
-           with Import     => True,
-                Convention => JavaScript_Function,
-                Link_Name  => "getContext";
+           with Import         => True,
+                Convention     => JavaScript_Function,
+                External_Name  => "getContext";
+   --  Returns an object that exposes an API for drawing on the canvas. The
+   --  first argument specifies the desired API, either "2d" or "webgl".
+   --  Subsequent arguments are handled by that API.
+   --
+   --  This specification defines the "2d" context below. There is also a
+   --  specification that defines a "webgl" context. [WEBGL]
+   --
+   --  Returns null if the given context ID is not supported, if the canvas has
+   --  already been initialised with the other context type (e.g.  trying to
+   --  get a "2d" context after getting a "webgl" context).
+   --
+   --  Throws an InvalidStateError exception if the setContext() or
+   --  transferControlToProxy() methods have been used.
+
+   not overriding function Probably_Supports_Context
+    (Self       : not null access HTML_Canvas_Element;
+     Context_Id : League.Strings.Universal_String) return Boolean is abstract
+       with Import        => True,
+            Convention    => JavaScript_Function,
+            External_Name => "probablySupportsContext";
+   --  Returns false if calling getContext() with the same arguments would
+   --  definitely return null, and true otherwise.
+   --
+   --  This return value is not a guarantee that getContext() will or will not
+   --  return an object, as conditions (e.g. availability of system resources)
+   --  can vary over time.
+   --
+   --  Throws an InvalidStateError exception if the setContext() or
+   --  transferControlToProxy() methods have been used.
 
 end WebAPI.HTML.Canvas_Elements;
