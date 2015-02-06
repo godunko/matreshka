08890cb30774cf67c1031e9ccb02f199554729f8
Maxim Reznik <reznikmm@gmail.com>
2015-02-06 19:41:27 +0000

    Avoid usage reserved words as identifiers. Use === as equal operator

--- design/asis2js/properties-declarations-defining_names.adb
+++ design/asis2js/properties-declarations-defining_names.adb
@@ -1,5 +1,9 @@
+with Ada.Containers.Hashed_Sets;
+
 with Asis.Declarations;
 with Asis.Elements;
+
+with League.Strings.Hash;
 
 with Properties.Tools;
 
@@ -8,6 +12,14 @@ package body Properties.Declarations.Defining_Names is
    function Link_Name
      (Element : Asis.Defining_Name)
       return League.Strings.Universal_String;
+
+   package String_Maps is new Ada.Containers.Hashed_Sets
+     (Element_Type        => League.Strings.Universal_String,
+      Hash                => League.Strings.Hash,
+      Equivalent_Elements => League.Strings."=",
+      "="                 => League.Strings."=");
+
+   Reserved : String_Maps.Set;
 
    ----------
    -- Code --
@@ -22,12 +34,16 @@ package body Properties.Declarations.Defining_Names is
       L_Name : constant League.Strings.Universal_String := Link_Name (Element);
       Image : constant Wide_String :=
         Asis.Declarations.Defining_Name_Image (Element);
-      Text : constant League.Strings.Universal_String :=
-        League.Strings.From_UTF_16_Wide_String (Image);
+      Text : League.Strings.Universal_String :=
+        League.Strings.From_UTF_16_Wide_String (Image).To_Lowercase;
       Value : League.Holders.Holder;
    begin
       if L_Name.Is_Empty then
-         Value := League.Holders.To_Holder (Text.To_Lowercase);
+         if Reserved.Contains (Text) then
+            Text.Prepend ("_");
+         end if;
+
+         Value := League.Holders.To_Holder (Text);
       else
          Value := League.Holders.To_Holder (L_Name);
       end if;
@@ -50,4 +66,6 @@ package body Properties.Declarations.Defining_Names is
       return League.Strings.From_UTF_16_Wide_String (Value);
    end Link_Name;
 
+begin
+   Reserved.Insert (League.Strings.To_Universal_String ("self"));
 end Properties.Declarations.Defining_Names;
