155ed349a085d5c3db71c03e3e1f818a315fdf13
Vadim Godunko <vgodunko@gmail.com>
2015-05-12 13:17:10 +0000

    Update test driver.

--- design/asis2js/testsuite/run.sh
+++ design/asis2js/testsuite/run.sh
@@ -8,17 +8,15 @@ PATH=$PATH:$HOME/node_modules/.bin/
 
 compile()
 {
-    $A2JS "-CA -T$WORK_DIR/.objs" $WORK_DIR/$1 > $WORK_DIR/aaa.js
-
-    js-beautify $WORK_DIR/aaa.js > $WORK_DIR/bbb.js
-
-    NAME=`head -n 1 $WORK_DIR/bbb.js | sed -e "s/.*'\([^']*\)', \[.*/\1/"`
-    mv $WORK_DIR/bbb.js $WORK_DIR/${NAME}.js
+    NAME=standard.${1//-/.}
+    NAME=${NAME%.*}.js
+    js-beautify $WORK_DIR/.objs/$NAME > $WORK_DIR/$NAME
 }
 
 mkdir $WORK_DIR
+export PATH=`pwd`/..:$PATH
 gnatchop $TEST $WORK_DIR
-gprbuild -p -P test.gpr -XWORK_DIR=$WORK_DIR
+gprbuild -p -P test.gpr -XWORK_DIR=$WORK_DIR --db ../gprconfig
 cp ../library/*.js $WORK_DIR
 sed --quiet -e '/EXPECTED OUTPUT:/,/END OF EXPECTED OUTPUT/p' $TEST \
     | sed -e '1d' -e '$d' -e 's/^--  //' \
