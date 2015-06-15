b5c542981df9123b3695d07a7edb65d94c0bfbf5
Vadim Godunko <vgodunko@gmail.com>
2015-06-15 08:46:42 +0000

    Use page generator to generate list of forums.

--- design/forum/server-servlets-forum_servlets.adb
+++ design/forum/server-servlets-forum_servlets.adb
@@ -1,23 +1,51 @@
-with Ada.Wide_Wide_Text_IO;
-
-with XML.SAX.Error_Handlers;
-with XML.SAX.Event_Writers;
-with XML.SAX.HTML5_Writers;
-with XML.SAX.Input_Sources.Streams.Files;
-with XML.SAX.Output_Destinations.Strings;
-with XML.SAX.Parse_Exceptions;
-with XML.SAX.Simple_Readers;
-with XML.Templates.Processors;
-with XML.Templates.Streams.Holders;
-with Servlet.Contexts;
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2015, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
 with League.Holders.JSON_Arrays;
 with League.IRIs;
 with League.JSON.Arrays;
 with League.JSON.Objects;
 with League.JSON.Values;
-with League.String_Vectors;
-
-with Forum.Categories;
 
 package body Server.Servlets.Forum_Servlets is
 
@@ -25,26 +53,43 @@ package body Server.Servlets.Forum_Servlets is
     (Item : Wide_Wide_String) return League.Strings.Universal_String
        renames League.Strings.To_Universal_String;
 
-   type Error_Handler is
-     limited new XML.SAX.Error_Handlers.SAX_Error_Handler with record
-      null;
-   end record;
+   ---------------------
+   -- Bind_Parameters --
+   ---------------------
 
-   overriding function Error_String
-    (Self : Error_Handler) return League.Strings.Universal_String;
-   --  Returns error message when processing was terminated by error handler.
+   overriding procedure Bind_Parameters
+    (Self      : in out Forum_List_Page;
+     Processor : in out XML.Templates.Processors.Template_Processor'Class)
+   is
+      List : League.JSON.Arrays.JSON_Array;
 
-   overriding procedure Fatal_Error
-    (Self       : in out Error_Handler;
-     Occurrence : XML.SAX.Parse_Exceptions.SAX_Parse_Exception);
-   --  Process fatal error in XML data. Outputs message to standard error.
+   begin
+      for J of Self.Categories loop
+         declare
+            Object : League.JSON.Objects.JSON_Object;
+            IRI    : League.IRIs.IRI;
 
-   procedure Get_Categories
-     (Server   : in out Forum.Forums.Servers.Server_Forum;
-      Response : in out Servlet.HTTP_Responses.HTTP_Servlet_Response'Class;
-      Path     : League.String_Vectors.Universal_String_Vector;
-      Filter   : in out XML.Templates.Processors.Template_Processor;
-      Template : out League.Strings.Universal_String);
+         begin
+            IRI.Set_Absolute_Path (Self.Path);
+            IRI.Append_Segment
+              (Forum.Categories.Encode (J.Object.Get_Identifier));
+
+            Object.Insert
+              (+"id",
+               League.JSON.Values.To_JSON_Value (IRI.To_Universal_String));
+            Object.Insert
+              (+"title",
+               League.JSON.Values.To_JSON_Value (J.Object.Get_Title));
+            Object.Insert
+              (+"description",
+               League.JSON.Values.To_JSON_Value (J.Object.Get_Description));
+            List.Append (Object.To_JSON_Value);
+         end;
+      end loop;
+
+      Processor.Set_Parameter
+        (+"list", League.Holders.JSON_Arrays.To_Holder (List));
+   end Bind_Parameters;
 
    ------------
    -- Do_Get --
@@ -55,144 +100,62 @@ package body Server.Servlets.Forum_Servlets is
       Request  : Servlet.HTTP_Requests.HTTP_Servlet_Request'Class;
       Response : in out Servlet.HTTP_Responses.HTTP_Servlet_Response'Class)
    is
-      Context   : constant access Servlet.Contexts.Servlet_Context'Class
-        := Request.Get_Servlet_Context;
-      Path      : League.String_Vectors.Universal_String_Vector
+      Path : League.String_Vectors.Universal_String_Vector
         := Request.Get_Servlet_Path;
-      Path_Info : constant League.String_Vectors.Universal_String_Vector
-        := Request.Get_Path_Info;
 
-      Input  : aliased XML.SAX.Input_Sources.Streams.Files.File_Input_Source;
-      Reader : XML.SAX.Simple_Readers.Simple_Reader;
-      Filter : aliased XML.Templates.Processors.Template_Processor;
-      Writer : aliased XML.SAX.HTML5_Writers.HTML5_Writer;
-      Output :
-        aliased XML.SAX.Output_Destinations.Strings.String_Output_Destination;
-      Error  : aliased Error_Handler;
-      Template : League.Strings.Universal_String;
    begin
-      --  Link components.
-
-      Reader.Set_Input_Source (Input'Unchecked_Access);
-      Reader.Set_Content_Handler (Filter'Unchecked_Access);
-      Reader.Set_Lexical_Handler (Filter'Unchecked_Access);
-      Reader.Set_Error_Handler (Error'Unchecked_Access);
-      Filter.Set_Content_Handler (Writer'Unchecked_Access);
-      Filter.Set_Lexical_Handler (Writer'Unchecked_Access);
-      Writer.Set_Output_Destination (Output'Unchecked_Access);
-
       Path.Prepend
         (League.String_Vectors.Universal_String_Vector'
            (Request.Get_Context_Path));
 
-      --  Set parameters
---      if Path_Info.Length = 0 then
-      Get_Categories
-        (Self.Server, Response, Path, Filter, Template);
----      end if;
-
-      Filter.Set_Parameter
-        (+"aaa", League.Holders.To_Holder
-           (+Natural'Wide_Wide_Image (Path_Info.Length)));
-
       Response.Set_Status (Servlet.HTTP_Responses.OK);
       Response.Set_Content_Type (+"text/html");
       Response.Set_Character_Encoding (+"UTF-8");
-
-      --  Process template file.
-
-      Input.Open_By_File_Name (Context.Get_Real_Path (Template));
-      Reader.Parse;
-
-      Response.Get_Output_Stream.Write (Output.Get_Text);
+      Response.Get_Output_Stream.Write
+       (Self.Forum_List.Render
+         (Request.Get_Session.all, Path, Self.Server.Get_Categories));
    end Do_Get;
-
-   ------------------
-   -- Error_String --
-   ------------------
-
-   overriding function Error_String
-    (Self : Error_Handler) return League.Strings.Universal_String is
-   begin
-      return League.Strings.Empty_Universal_String;
-   end Error_String;
-
-   -----------------
-   -- Fatal_Error --
-   -----------------
-
-   overriding procedure Fatal_Error
-    (Self       : in out Error_Handler;
-     Occurrence : XML.SAX.Parse_Exceptions.SAX_Parse_Exception)
-   is
-      Line   : constant Wide_Wide_String
-        := Integer'Wide_Wide_Image (Occurrence.Line);
-      Column : constant Wide_Wide_String
-        := Integer'Wide_Wide_Image (Occurrence.Column);
-
-   begin
-      Ada.Wide_Wide_Text_IO.Put_Line
-       (Ada.Wide_Wide_Text_IO.Standard_Error,
-        Occurrence.System_Id.To_Wide_Wide_String
-          & ':'
-          & Line (Line'First + 1 .. Line'Last)
-          & ':'
-          & Column (Column'First + 1 .. Column'Last)
-          & ": "
-          & Occurrence.Message.To_Wide_Wide_String);
-   end Fatal_Error;
-
-   --------------------
-   -- Get_Categories --
-   --------------------
-
-   procedure Get_Categories
-     (Server   : in out Forum.Forums.Servers.Server_Forum;
-      Response : in out Servlet.HTTP_Responses.HTTP_Servlet_Response'Class;
-      Path     : League.String_Vectors.Universal_String_Vector;
-      Filter   : in out XML.Templates.Processors.Template_Processor;
-      Template : out League.Strings.Universal_String)
-   is
-      Arr    : League.JSON.Arrays.JSON_Array;
-   begin
-      for J of Server.Get_Categories loop
-         declare
-            JSON : League.JSON.Objects.JSON_Object;
-            IRI  : League.IRIs.IRI;
-         begin
-            IRI.Set_Absolute_Path (Path);
-            IRI.Append_Segment
-              (Forum.Categories.Encode (J.Object.Get_Identifier));
-
-            JSON.Insert
-              (+"id",
-               League.JSON.Values.To_JSON_Value (IRI.To_Universal_String));
-            JSON.Insert
-              (+"title",
-               League.JSON.Values.To_JSON_Value (J.Object.Get_Title));
-            JSON.Insert
-              (+"description",
-               League.JSON.Values.To_JSON_Value (J.Object.Get_Description));
-            Arr.Append (JSON.To_JSON_Value);
-         end;
-      end loop;
-
-      Filter.Set_Parameter
-        (+"list", League.Holders.JSON_Arrays.To_Holder (Arr));
-
-      Template := +"/WEB-INF/templates/page.xhtml.tmpl";
-   end Get_Categories;
 
    ----------------------
    -- Get_Servlet_Info --
    ----------------------
 
    overriding function Get_Servlet_Info
-     (Self : Forum_Servlet)
-      return League.Strings.Universal_String
-   is
+    (Self : Forum_Servlet) return League.Strings.Universal_String is
    begin
       return League.Strings.To_Universal_String ("Forum Servlet");
    end Get_Servlet_Info;
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   overriding procedure Initialize
+    (Self   : in out Forum_Servlet;
+     Config : not null access Servlet.Configs.Servlet_Config'Class) is
+   begin
+      Servlet.HTTP_Servlets.HTTP_Servlet (Self).Initialize (Config);
+      Self.Forum_List.Initialize
+       (Config.Get_Servlet_Context,
+        +"/WEB-INF/templates/page.xhtml.tmpl",
+        +"/WEB-INF/templates/forum_list.xhtml.tmpl");
+   end Initialize;
+
+   ------------
+   -- Render --
+   ------------
+
+   function Render
+    (Self       : in out Forum_List_Page'Class;
+     Session    : Servlet.HTTP_Sessions.HTTP_Session'Class;
+     Path       : League.String_Vectors.Universal_String_Vector;
+     Categories : Forum.Categories.References.Category_Vector)
+       return League.Strings.Universal_String is
+   begin
+      Self.Categories := Categories;
+      Self.Path       := Path;
+
+      return Self.Render (Session);
+   end Render;
 
 end Server.Servlets.Forum_Servlets;
--- design/forum/server-servlets-forum_servlets.ads
+++ design/forum/server-servlets-forum_servlets.ads
@@ -1,16 +1,84 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2015, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with League.String_Vectors;
 with League.Strings;
+with XML.Templates.Processors;
 
+with Servlet.Configs;
 with Servlet.HTTP_Requests;
 with Servlet.HTTP_Responses;
 with Servlet.HTTP_Servlets;
+with Servlet.HTTP_Sessions;
 
+with AWFC.Page_Generators;
+
+with Forum.Categories.References;
 with Forum.Forums.Servers;
 
 package Server.Servlets.Forum_Servlets is
 
+   type Forum_List_Page is
+     new AWFC.Page_Generators.Abstract_Page_Generator with record
+      Path       : League.String_Vectors.Universal_String_Vector;
+      Categories : Forum.Categories.References.Category_Vector;
+   end record;
+
+   function Render
+    (Self       : in out Forum_List_Page'Class;
+     Session    : Servlet.HTTP_Sessions.HTTP_Session'Class;
+     Path       : League.String_Vectors.Universal_String_Vector;
+     Categories : Forum.Categories.References.Category_Vector)
+       return League.Strings.Universal_String;
+
+   overriding procedure Bind_Parameters
+    (Self      : in out Forum_List_Page;
+     Processor : in out XML.Templates.Processors.Template_Processor'Class);
+
    type Forum_Servlet is
      new Servlet.HTTP_Servlets.HTTP_Servlet with record
-      Server : Forum.Forums.Servers.Server_Forum;
+      Server     : Forum.Forums.Servers.Server_Forum;
+      Forum_List : Forum_List_Page;
    end record;
 
    type Forum_Servlet_Access is access all Forum_Servlet'Class;
@@ -22,5 +90,9 @@ package Server.Servlets.Forum_Servlets is
 
    overriding function Get_Servlet_Info
     (Self : Forum_Servlet) return League.Strings.Universal_String;
+
+   overriding procedure Initialize
+    (Self   : in out Forum_Servlet;
+     Config : not null access Servlet.Configs.Servlet_Config'Class);
 
 end Server.Servlets.Forum_Servlets;
