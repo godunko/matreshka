8a9e0156463bac9e986e712b5614ea041e1a4f05
Maxim Reznik <reznikmm@gmail.com>
2016-03-17 11:59:57 +0000

    Add SQL Database connection pools

---
+++ design/pools/sql-databases-pools.adb
@@ -0,0 +1,333 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                           SQL Database Access                            --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2011-2012, Vadim Godunko <vgodunko@gmail.com>                --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+package body SQL.Databases.Pools is
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create
+     (Driver   : League.Strings.Universal_String;
+      Options  : SQL.Options.SQL_Options;
+      Max_Size : Positive)
+      return SQL_Connection_Pool
+   is
+      Size    : constant Idem_Index := Idem_Index (Max_Size);
+      Modulus : constant Ada.Containers.Hash_Type :=
+        Hashed_Maps.Default_Modulus (Size);
+   begin
+      return Value : SQL_Connection_Pool (Size, Modulus) do
+         Value.Driver := Driver;
+         Value.Options := Options;
+         Value.Index_Map.Initialize;
+      end return;
+   end Create;
+
+   ------------
+   -- Create --
+   ------------
+
+   overriding function Create
+     (Driver  : League.Strings.Universal_String;
+      Options : SQL.Options.SQL_Options)
+      return SQL_Database is
+   begin
+      raise Program_Error;
+      return Create (Driver, Options);
+   end Create;
+
+   --------------
+   -- Finalize --
+   --------------
+
+   overriding procedure Finalize (Self : in out SQL_Database) is
+   begin
+      if Self.Index /= 0 then
+         Self.Pool.Items (Self.Index).Release_Connection
+           (Self.Data, Self.Options);
+         Self.Pool.Index_Map.Release_Index (Self.Index);
+         Self.Data := null;
+         Self.Index := 0;
+      else
+         SQL.Databases.SQL_Database (Self).Finalize;
+      end if;
+   end Finalize;
+
+   ---------
+   -- Get --
+   ---------
+
+   function Get
+     (Self    : aliased in out SQL_Connection_Pool'Class;
+      Options : SQL.Options.SQL_Options;
+      Error   : out League.Strings.Universal_String)
+      return SQL_Database
+   is
+      use type SQL.Options.SQL_Options;
+      use type Matreshka.Internals.SQL_Drivers.Database_Access;
+
+      Result  : Matreshka.Internals.SQL_Drivers.Database_Access;
+      Index   : Idem_Index;
+      Present : SQL.Options.SQL_Options;
+   begin
+      Self.Index_Map.Get_Index (Options, Index);
+      Self.Items (Index).Get_Connection (Result, Present);
+
+      if Present /= Options then
+         declare
+            Be : Matreshka.Internals.SQL_Drivers.Database_Access :=
+              Matreshka.Internals.SQL_Drivers.Create (Self.Driver);
+         begin
+            if not Be.Open (Options) then
+               Error := Be.Error_Message;
+               Matreshka.Internals.SQL_Drivers.Dereference (Be);
+               Self.Items (Index).Release_Connection (Result, Present);
+               Self.Index_Map.Release_Unused_Index (Present, Index);
+
+               return
+                 (Ada.Finalization.Limited_Controlled with
+                  Self'Access,
+                  Matreshka.Internals.SQL_Drivers.Dummy.Empty_Database'Access,
+                  Options,
+                  Index => 0);
+            end if;
+
+            if Result /= null then
+               Result.Close;
+               Matreshka.Internals.SQL_Drivers.Dereference (Result);
+            end if;
+
+            Result := Be;
+         end;
+      end if;
+
+      return
+        (Ada.Finalization.Limited_Controlled with
+           Data    => Result,
+           Options => Options,
+           Pool    => Self'Access,
+           Index   => Index);
+   end Get;
+
+   -----------------------
+   -- Locked_Hashed_Map --
+   -----------------------
+
+   protected body Locked_Hashed_Map is
+
+      procedure Append_Free_Item (Index : Idem_Index);
+      procedure Drop_Free_Item (Index : Idem_Index);
+
+      ----------------------
+      -- Append_Free_Item --
+      ----------------------
+
+      procedure Append_Free_Item (Index : Idem_Index) is
+         Last : Idem_Index;
+      begin
+         if Free = 0 then
+            Free_List (Index) := (Prev => Index, Next => Index);
+         else
+            Last := Free_List (Free).Prev;
+
+            Free_List (Index).Prev := Last;
+            Free_List (Last).Next := Index;
+
+            Free_List (Index).Next := Free;
+            Free_List (Free).Prev := Index;
+         end if;
+      end Append_Free_Item;
+
+      --------------------
+      -- Drop_Free_Item --
+      --------------------
+
+      procedure Drop_Free_Item (Index : Idem_Index) is
+         X : Free_List_Item renames Free_List (Index);
+      begin
+         if X.Next = Index then
+            Free := 0;
+
+         else
+            Free_List (X.Prev).Next := X.Next;
+            Free_List (X.Next).Prev := X.Prev;
+
+            if Free = Index then
+               Free := X.Next;
+            end if;
+         end if;
+      end Drop_Free_Item;
+
+      ----------------
+      -- Initialize --
+      ----------------
+
+      procedure Initialize is
+      begin
+         for J in 1 .. Max_Size loop
+            Append_Free_Item (J);
+         end loop;
+      end Initialize;
+
+      ---------------
+      -- Get_Index --
+      ---------------
+
+      entry Get_Index
+        (Value : SQL.Options.SQL_Options;
+         Index : out Idem_Index) when True
+      is
+         Cursor : constant Hashed_Maps.Cursor := Map.Find (Value);
+      begin
+         if Hashed_Maps.Has_Element (Cursor) then
+            Index := Hashed_Maps.Element (Cursor);
+
+            if Count (Index) = 0 then
+               Drop_Free_Item (Index);
+            end if;
+
+            Count (Index) := Count (Index) + 1;
+         else
+            requeue Get_Free_Index;
+         end if;
+      end Get_Index;
+
+      --------------------
+      -- Get_Free_Index --
+      --------------------
+
+      entry Get_Free_Index
+        (Value : SQL.Options.SQL_Options;
+         Index : out Idem_Index) when Free /= 0
+      is
+      begin
+         Index := Free;
+         Drop_Free_Item (Index);
+         Count (Index) := 1;
+
+         if Used (Index) then
+            Map.Delete (Option (Index));
+         end if;
+
+         Map.Insert (Value, Index);
+         Option (Index) := Value;
+      end Get_Free_Index;
+
+      -------------------
+      -- Release_Index --
+      -------------------
+
+      procedure Release_Index (Index : Idem_Index) is
+         Value  : constant SQL.Options.SQL_Options := Option (Index);
+      begin
+         Count (Index) := Count (Index) - 1;
+
+         if Count (Index) = 0 then
+            Append_Free_Item (Index);
+            Used (Index) := True;
+         end if;
+      end Release_Index;
+
+      --------------------------
+      -- Release_Unused_Index --
+      --------------------------
+
+      procedure Release_Unused_Index
+        (Value : SQL.Options.SQL_Options;
+         Index : Idem_Index) is
+      begin
+         pragma Assert (Count (Index) = 1);
+
+         Count (Index) := Count (Index) - 1;
+         Append_Free_Item (Index);
+         Free := Index;
+         Map.Delete (Option (Index));
+
+         if Used (Index) then
+            Option (Index) := Value;
+            Map.Insert (Value, Index);
+         end if;
+      end Release_Unused_Index;
+
+   end Locked_Hashed_Map;
+
+   ---------------
+   -- Pool_Item --
+   ---------------
+
+   protected body Pool_Item is
+
+      --------------------
+      -- Get_Connection --
+      --------------------
+
+      entry Get_Connection
+        (Value   : out Matreshka.Internals.SQL_Drivers.Database_Access;
+         Options : out SQL.Options.SQL_Options)
+         when not Busy
+      is
+      begin
+         Busy := True;
+         Value := Connect;
+         Options := Pool_Item.Options;
+      end Get_Connection;
+
+      ------------------------
+      -- Release_Connection --
+      ------------------------
+
+      procedure Release_Connection
+        (Value   : Matreshka.Internals.SQL_Drivers.Database_Access;
+         Options : SQL.Options.SQL_Options)
+      is
+      begin
+         Busy := True;
+         Pool_Item.Options := Options;
+         Connect := Value;
+      end Release_Connection;
+
+   end Pool_Item;
+
+end SQL.Databases.Pools;
---
+++ design/pools/sql-databases-pools.ads
@@ -0,0 +1,178 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                           SQL Database Access                            --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2011-2012, Vadim Godunko <vgodunko@gmail.com>                --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+private with Ada.Containers.Bounded_Hashed_Maps;
+private with SQL.Options.Hash;
+
+package SQL.Databases.Pools is
+
+   type SQL_Database (<>) is new SQL.Databases.SQL_Database with private;
+   --  Database connection from some connection pool
+
+   type SQL_Connection_Pool (<>) is tagged limited private;
+   --  Connection pool aggregates several DB connection
+
+   function Create
+    (Driver   : League.Strings.Universal_String;
+     Options  : SQL.Options.SQL_Options;
+     Max_Size : Positive) return SQL_Connection_Pool;
+   --  Create new connection pool of given size
+
+   function Get
+     (Self    : aliased in out SQL_Connection_Pool'Class;
+      Options : SQL.Options.SQL_Options;
+      Error   : out League.Strings.Universal_String)
+     return SQL_Database;
+   --  Open new database connection or reuse some if it's already in the pool.
+   --  If connection fails return not empty Error message
+
+private
+
+   subtype Idem_Index is
+     Ada.Containers.Count_Type range 1 .. Ada.Containers.Count_Type'Last;
+
+   use type Idem_Index;
+
+   package Hashed_Maps is new Ada.Containers.Bounded_Hashed_Maps
+     (Key_Type        => SQL.Options.SQL_Options,
+      Element_Type    => Idem_Index,
+      Hash            => SQL.Options.Hash,
+      Equivalent_Keys => SQL.Options."=",
+      "="             => "=");
+
+   --  Representation of double linked list link of Idem_Index
+   type Free_List_Item is record
+      Prev : Idem_Index;
+      Next : Idem_Index;
+   end record;
+
+   type Free_List_Array is array (Idem_Index range <>) of Free_List_Item;
+   type Boolean_Array is array (Idem_Index range <>) of Boolean;
+   type Natural_Array is array (Idem_Index range <>) of Natural;
+   type Options_Array is
+     array (Idem_Index range <>) of SQL.Options.SQL_Options;
+
+   --  Protected map from options to item index
+   protected type Locked_Hashed_Map
+     (Max_Size : Idem_Index;
+      Modulus  : Ada.Containers.Hash_Type) is
+
+      procedure Initialize;
+
+      entry Get_Index
+        (Value : SQL.Options.SQL_Options;
+         Index : out Idem_Index);
+      --  Reserve some Index in 1 .. Max_Size for use with given option set
+
+      entry Get_Free_Index
+        (Value : SQL.Options.SQL_Options;
+         Index : out Idem_Index);
+      --  Reserve some Index as it is beeing released
+
+      procedure Release_Index (Index : Idem_Index);
+      --  Given Index not used by this task any more
+
+      procedure Release_Unused_Index
+        (Value : SQL.Options.SQL_Options;
+         Index : Idem_Index);
+      --  Return Index back when it was not used. Assign given options to it
+
+   private
+      Map       : Hashed_Maps.Map (Max_Size, Modulus);
+      --  Mapping from Options to corresponding index
+      Free      : Idem_Index'Base := 0;
+      --  First element in Free_List
+      Free_List : Free_List_Array (1 .. Max_Size);
+      --  List of Options with Count=0 in LRU order
+      Count     : Natural_Array (1 .. Max_Size) := (others => 0);
+      --  Count of allocated indexes
+      Option    : Options_Array (1 .. Max_Size);
+      --  Map from index to Options if Used = True
+      Used      : Boolean_Array (1 .. Max_Size) := (others => False);
+      --  Flag if given index was assigned to Options at least once.
+      --  Valid only for indexes with Count=0
+   end Locked_Hashed_Map;
+
+   protected type Pool_Item is
+
+      entry Get_Connection
+        (Value   : out Matreshka.Internals.SQL_Drivers.Database_Access;
+         Options : out SQL.Options.SQL_Options);
+      --  Get database and corresponding option set
+
+      procedure Release_Connection
+        (Value   : Matreshka.Internals.SQL_Drivers.Database_Access;
+         Options : SQL.Options.SQL_Options);
+      --  Return database to the item
+   private
+      Connect : Matreshka.Internals.SQL_Drivers.Database_Access;
+      Options : SQL.Options.SQL_Options;
+      Busy    : Boolean := False;
+   end Pool_Item;
+
+   type Pool_Item_Array is array (Idem_Index range <>) of Pool_Item;
+
+   type SQL_Connection_Pool
+     (Max_Size : Idem_Index;
+      Modulus  : Ada.Containers.Hash_Type) is tagged limited
+   record
+      Driver    : League.Strings.Universal_String;
+      Options   : SQL.Options.SQL_Options;
+      Index_Map : Locked_Hashed_Map (Max_Size, Modulus);
+      Items     : Pool_Item_Array (1 .. Max_Size);
+   end record;
+
+   type SQL_Database (Pool : not null access SQL_Connection_Pool'Class) is
+     new SQL.Databases.SQL_Database with
+   record
+      Index : Idem_Index'Base := 0;
+   end record;
+
+   overriding procedure Finalize (Self : in out SQL_Database);
+
+   overriding function Create
+    (Driver  : League.Strings.Universal_String;
+     Options : SQL.Options.SQL_Options) return SQL_Database;
+
+end SQL.Databases.Pools;
