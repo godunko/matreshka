6d05aa39655320575b3f47436edaf00730370a2d
Maxim Reznik <reznikmm@gmail.com>
2016-09-27 14:09:05 +0000

    Implement error handler

--- design/xhtml2html5/xhtml2html5.adb
+++ design/xhtml2html5/xhtml2html5.adb
@@ -42,18 +42,94 @@
 --  $Revision$ $Date$
 ------------------------------------------------------------------------------
 
+with Ada.Command_Line;
 with Ada.Wide_Wide_Text_IO;
 
 with League.Application;
 with League.String_Vectors;
 with League.Strings;
 
+with XML.SAX.Error_Handlers;
 with XML.SAX.HTML5_Writers;
 with XML.SAX.Input_Sources.Streams.Files;
 with XML.SAX.Output_Destinations.Strings;
+with XML.SAX.Parse_Exceptions;
 with XML.SAX.Simple_Readers;
 
 procedure XHTML2HTML5 is
+   package Error_Handlers is
+      type Error_Handler is new XML.SAX.Error_Handlers.SAX_Error_Handler with
+         record
+            Clean : Boolean := True;
+         end record;
+
+      overriding procedure Error
+       (Self       : in out Error_Handler;
+        Occurrence : XML.SAX.Parse_Exceptions.SAX_Parse_Exception;
+        Success    : in out Boolean);
+
+      overriding function Error_String
+       (Self : Error_Handler)
+        return League.Strings.Universal_String is
+        (League.Strings.Empty_Universal_String);
+
+      overriding procedure Fatal_Error
+       (Self       : in out Error_Handler;
+        Occurrence : XML.SAX.Parse_Exceptions.SAX_Parse_Exception);
+
+   end Error_Handlers;
+
+   --------------------
+   -- Error_Handlers --
+   --------------------
+
+   package body Error_Handlers is
+
+      -----------
+      -- Error --
+      -----------
+
+      overriding procedure Error
+       (Self       : in out Error_Handler;
+        Occurrence : XML.SAX.Parse_Exceptions.SAX_Parse_Exception;
+        Success    : in out Boolean) is
+      begin
+         Ada.Wide_Wide_Text_IO.Put_Line
+           (Ada.Wide_Wide_Text_IO.Standard_Error,
+            "Error:");
+         Ada.Wide_Wide_Text_IO.Put_Line
+           (Ada.Wide_Wide_Text_IO.Standard_Error,
+            Occurrence.Message.To_Wide_Wide_String);
+         Ada.Wide_Wide_Text_IO.Put_Line
+           (Ada.Wide_Wide_Text_IO.Standard_Error,
+            "  at " & Natural'Wide_Wide_Image (Occurrence.Line) &
+            ":" & Natural'Wide_Wide_Image (Occurrence.Column));
+         Ada.Wide_Wide_Text_IO.Put_Line
+           (Ada.Wide_Wide_Text_IO.Standard_Error,
+            "  of " & Occurrence.System_Id.To_Wide_Wide_String &
+                " - " & Occurrence.Public_Id.To_Wide_Wide_String);
+         Ada.Wide_Wide_Text_IO.New_Line
+           (Ada.Wide_Wide_Text_IO.Standard_Error);
+      end Error;
+
+      -----------------
+      -- Fatal_Error --
+      -----------------
+
+      overriding procedure Fatal_Error
+       (Self       : in out Error_Handler;
+        Occurrence : XML.SAX.Parse_Exceptions.SAX_Parse_Exception)
+      is
+         Success : Boolean := True;
+      begin
+         Ada.Wide_Wide_Text_IO.Put
+           (Ada.Wide_Wide_Text_IO.Standard_Error,
+            "Fatal_");
+         Self.Error (Occurrence, Success);
+      end Fatal_Error;
+
+   end Error_Handlers;
+
    Args   : constant League.String_Vectors.Universal_String_Vector :=
         League.Application.Arguments;
    Input  : aliased XML.SAX.Input_Sources.Streams.Files.File_Input_Source;
@@ -61,13 +137,23 @@ procedure XHTML2HTML5 is
    Writer : aliased XML.SAX.HTML5_Writers.HTML5_Writer;
    Output : aliased XML.SAX.Output_Destinations.Strings
      .String_Output_Destination;
-   Errors : League.Strings.Universal_String;
+   Error  : aliased Error_Handlers.Error_Handler;
+
 begin
    Input.Open_By_File_Name (Args.Element (1));
    Reader.Set_Input_Source (Input'Unchecked_Access);
    Reader.Set_Content_Handler (Writer'Unchecked_Access);
    Reader.Set_Lexical_Handler (Writer'Unchecked_Access);
+   Reader.Set_Error_Handler (Error'Unchecked_Access);
    Writer.Set_Output_Destination (Output'Unchecked_Access);
+--   Writer.Set_Error_Handler (Error'Unchecked_Access);
    Reader.Parse;
-   Ada.Wide_Wide_Text_IO.Put (Output.Get_Text.To_Wide_Wide_String);
+
+   if Error.Clean then
+
+      Ada.Wide_Wide_Text_IO.Put (Output.Get_Text.To_Wide_Wide_String);
+   else
+
+      Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
+   end if;
 end XHTML2HTML5;
