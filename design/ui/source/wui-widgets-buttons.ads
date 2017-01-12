8d54eb1725070f1cf16a52af4e855648101181b7
Vadim Godunko <vgodunko@gmail.com>
2017-01-12 17:08:40 +0000

    Add minimal implementation of push button.

---
+++ design/ui/source/wui-widgets-buttons.ads
@@ -0,0 +1,76 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                            Matreshka Project                             --
+--                                                                          --
+--                               Web Framework                              --
+--                                                                          --
+--                        Runtime Library Component                         --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2017, Vadim Godunko <vgodunko@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Vadim Godunko, IE nor the names of its        --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+with Core.Connectables.Slots;
+private with Core.Connectables.Slots.Signals;
+
+package WUI.Widgets.Buttons is
+
+   type Abstract_Button is
+     abstract new WUI.Widgets.Abstract_Widget with private;
+
+   not overriding function Clicked_Signal
+    (Self : in out Abstract_Button)
+       return not null access Core.Connectables.Slots.Connection_Manager'Class;
+
+   package Constructors is
+
+      procedure Initialize
+       (Self    : in out Abstract_Button'Class;
+        Element : not null WebAPI.HTML.Elements.HTML_Element_Access);
+
+   end Constructors;
+
+private
+
+   type Abstract_Button is
+     abstract new WUI.Widgets.Abstract_Widget with record
+      Clicked : aliased Core.Connectables.Slots.Signals.Signal
+                         (Abstract_Button'Unchecked_Access);
+   end record;
+
+   overriding procedure Click_Event
+    (Self  : in out Abstract_Button;
+     Event : in out WUI.Events.Mouse.Click.Click_Event'Class);
+
+end WUI.Widgets.Buttons;
